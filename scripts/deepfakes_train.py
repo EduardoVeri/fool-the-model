debbe8cf5ef3e7f739c153a43ec6fc03f9fa5a90
eduardoveri <verissimo.eduardo@unifesp.br>
2024-12-15 21:04:15 -0300

    [STYLE] Add comments

--- scripts/deepfakes_train.py
+++ scripts/deepfakes_train.py
@@ -83,21 +83,25 @@ class CNN(nn.Module):
     def __init__(self, num_classes=2):
         super(CNN, self).__init__()
         self.features = nn.Sequential(
+            # First Layer
             nn.Conv2d(3, 32, kernel_size=3, padding=1),
             nn.BatchNorm2d(32),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2),
             nn.Dropout(0.05),
+            # Second Layer
             nn.Conv2d(32, 64, kernel_size=3, padding=1),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2),
             nn.Dropout(0.05),
+            # Third Layer
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.1),
             nn.MaxPool2d(2),
             nn.Dropout(0.05),
+            # Fourth Layer
             nn.Conv2d(128, 256, kernel_size=3, padding=1),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.1),
@@ -105,14 +109,18 @@ class CNN(nn.Module):
             nn.Dropout(0.05),
         )
         self.classifier = nn.Sequential(
+            # Flatten the kernels output
             nn.AdaptiveAvgPool2d((1, 1)),
             nn.Flatten(),
+            # First MLP Layer
             nn.Linear(256, 128),
             nn.LeakyReLU(0.1),
             nn.Dropout(0.05),
+            # Second MLP Layer
             nn.Linear(128, 32),
             nn.LeakyReLU(0.1),
             nn.Dropout(0.05),
+            # Output Layer
             nn.Linear(32, num_classes),
         )
 
