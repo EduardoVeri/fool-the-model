6a6d5ce28ffbebaa359cda76e5372c1977aa7e33
eduardoveri <verissimo.eduardo@unifesp.br>
2025-01-21 20:55:02 -0300

    [FEAT] Enhance DeepFakeDataset with methods to retrieve fake and real images

--- adversarial_classifier/dataloader/dataloader.py
+++ adversarial_classifier/dataloader/dataloader.py
@@ -9,14 +9,14 @@ class DeepFakeDataset(Dataset):
         self.data = pd.read_csv(csv_file)
         self.root_dir = root_dir
         self.transform = transform
-        self.data = self.data.sample(frac=fraction).reset_index(drop=True)
+        if fraction < 1:
+            self.data = self.data.sample(frac=fraction).reset_index(drop=True)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.root_dir, self.data.iloc[idx]["path"])
-        image = io.imread(img_path)
+        image = self.__open_image(idx)
         label = self.data.iloc[idx]["label"]
 
         if self.transform:
@@ -24,6 +24,30 @@ class DeepFakeDataset(Dataset):
 
         return image, label
 
+    def __open_image(self, idx):
+        img_path = os.path.join(self.root_dir, self.data.iloc[idx]["path"])
+        image = io.imread(img_path)
+        return image
+    
+    def get_fake_image(self, idx):
+        fake_images = self.data[self.data["label"] == 0].reset_index(drop=True)
+        fake_images.sort_values("path", inplace=True)
+        image = self.__open_image(fake_images.iloc[idx].name)
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, 0
+
+    def get_real_image(self, idx):
+        real_images = self.data[self.data["label"] == 1].reset_index(drop=True).sort_values("path")
+        image = self.__open_image(real_images.iloc[idx].name)
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        return image, 1
+    
 
 def get_dataloaders(path: str, batch_size: int, transform: object, fraction: float = 1):
     train_dataset = DeepFakeDataset(
